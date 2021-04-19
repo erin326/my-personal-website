b48ebe2f944107835d7282fd75987fd02e62ee2b
erin <erinverkoulen93@gmail.com>
2021-04-18 20:10:25 -0500

    created html content, css properties, click fn on header

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-const h1 = document.createElement('h1');
+/*const h1 = document.createElement('h1');
 h1.textContent = "Skills";
 document.querySelector('body').appendChild(h1);
+*/
+const header = document.querySelector('#header');
+
+
+header.addEventListener('click', function(){
+alert('Ouch!');
+});
