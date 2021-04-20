350494ca8ddf3abcfd1797ab21571d7e716be276
erin <erinverkoulen93@gmail.com>
2021-04-19 22:22:25 -0500

    html links, img-1, click alerts edited,css hand

--- script.js
+++ script.js
@@ -2,9 +2,21 @@
 h1.textContent = "Skills";
 document.querySelector('body').appendChild(h1);
 */
+
 const header = document.querySelector('#header');
 
+const button = document.querySelector('#btn');
 
-header.addEventListener('click', function(){
-alert('Ouch!');
+const skills = document.querySelector('#skills');
+
+function clickAlert1() {
+        alert('You are beautiful!');
+}
+
+header.addEventListener('click', function() {
+    alert("Those eyes are to die for!");
 });
+
+
+button.addEventListener('click', clickAlert1);
+
